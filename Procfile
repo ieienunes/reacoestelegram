b2a6b91e639354c5f7d58b85683461e2eaa1946c
ieienunes <42755033+ieienunes@users.noreply.github.com>
2024-05-28 12:09:18 -0300

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+bot: python reactionbot.py
---
+++ config.py
@@ -0,0 +1,17 @@
+# If you want to add more than one channel then:
+
+CHANNELS = ['user do grupo']
+
+POSSIBLE_KEY_NAMES = {
+    'api_id': ['api_id', 'app_id'],
+    'api_hash': ['api_hash', 'app_hash'],
+    'app_version': ['app_version'],
+    'device_model': ['device_model', 'device'],
+    'system_version': ['system_version', 'sdk'],
+    'phone_number': ['phone_number', 'phone'],
+    'lang_code': ['lang_code'],
+    'ipv6': ['ipv6'],
+    'proxy': ['proxy'],
+}
+
+EMOJIS = ["👍", "❤️", "👏", "🔥", "😍", "🎉"]
